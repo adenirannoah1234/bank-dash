f3dedf44d88a0695711239f2af0eb2c00a302f1b
adenirannoah1234 <adenirannoah9@gmail.com>
2024-12-05 19:49:14 +0100

    created the topnav

---
+++ src/app/components/layoutContainer.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import React from 'react';
+import Sidebar from './Sidebar';
+import TopNav from './TopNav';
+
+export const LayoutContainer = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <nav className="flexShrink-0 h-screen ">
+        <Sidebar />
+      </nav>
+      <div className="flex flex-col w-full h-screen overflow-hidden flex-1">
+        <header className="flexShrink-0  ">
+          <TopNav />{' '}
+        </header>
+        {/* {isMobileMenuOpen && (
+          <Box
+            position="fixed"
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            bg="blackAlpha.600"
+            display={{ base: 'block', lg: 'none' }}
+            onClick={() => setIsMobileMenuOpen(false)}
+            zIndex={998}
+          />
+        )} */}
+        <main className="flex-1 overflow-y-auto min-w-0 p-4 bg-custom-gray">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'custom-gray': '#f5f7fa',
       },
     },
   },
